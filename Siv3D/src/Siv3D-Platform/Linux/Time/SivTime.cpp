cafb6613cb121703d6f77115e1a52025e649653e
yaito3014 <ykakeyama3014@gmail.com>
2024-03-02 16:31:35 +0000

    add blank definitions

---
+++ Siv3D/src/Siv3D-Platform/Linux/Time/SivTime.cpp
@@ -0,0 +1,68 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2024 Ryo Suzuki
+//	Copyright (c) 2016-2024 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/Time.hpp>
+
+namespace s3d
+{
+	namespace Time
+	{
+		int64 GetSec() noexcept
+		{
+			return -1;  // TODO: implement
+		}
+
+		int64 GetMillisec() noexcept
+		{
+			return -1;  // TODO: implement
+		}
+
+		int64 GetMicrosec() noexcept
+		{
+			return -1;  // TODO: implement
+		}
+
+		int64 GetNanosec() noexcept
+		{
+			return -1;  // TODO: implement
+		}
+
+		int64 GetUptimeSec() noexcept
+		{
+			return -1;  // TODO: implement
+		}
+
+		int64 GetUptimeMillisec() noexcept
+		{
+			return -1;  // TODO: implement
+		}
+
+		uint64 GetSecSinceEpoch() noexcept
+		{
+			return -1;  // TODO: implement
+		}
+
+		uint64 GetMillisecSinceEpoch() noexcept
+		{
+			return -1;  // TODO: implement
+		}
+
+		uint64 GetMicrosecSinceEpoch() noexcept
+		{
+			 return -1;  // TODO: implemen
+		}
+
+		int32 UTCOffsetMinutes() noexcept
+		{
+			return -1;  // TODO: implemen
+		}
+	}
+}
