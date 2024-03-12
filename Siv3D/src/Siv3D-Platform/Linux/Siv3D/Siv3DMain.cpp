b6a4f6aeebe315e357ad018d31fb5b9e8cf8b41d
yaito3014 <ykakeyama3014@gmail.com>
2024-03-12 17:06:20 +0900

    add sources for linux

---
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/Siv3DMain.cpp
@@ -0,0 +1,43 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2024 Ryo Suzuki
+//	Copyright (c) 2016-2024 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <iostream>
+# include <Siv3D/System/ISystem.hpp>
+# include <Siv3D/Error.hpp>
+# include <Siv3D/Engine/Siv3DEngine.hpp>
+# include <Siv3D/EngineLog.hpp>
+
+void Main();
+
+int main(int, char**)
+{
+	using namespace s3d;
+	
+	std::clog << "OpenSiv3D for Linux\n";
+	
+	Siv3DEngine engine;
+	
+	try
+	{
+		SIV3D_ENGINE(System)->init();
+	}
+	catch (const Error& error)
+	{
+		std::cerr << error << '\n';
+		return -1;
+	}
+
+	LOG_DEBUG("Main() ---");
+
+	Main();
+	
+	LOG_DEBUG("--- Main()");
+}
