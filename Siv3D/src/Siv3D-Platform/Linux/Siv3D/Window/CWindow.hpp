63c2f900a78f54b7f51f9781c10e0eaac34574be
yaito3014 <ykakeyama3014@gmail.com>
2024-07-07 02:04:02 +0900

    eliminate some errors

--- Siv3D/src/Siv3D-Platform/Linux/Siv3D/Window/CWindow.hpp
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/Window/CWindow.hpp
@@ -11,6 +11,7 @@
 
 # pragma once
 # include <Siv3D/Window.hpp>
+# include <Siv3D/Stopwatch.hpp>
 # include <Siv3D/WindowState.hpp>
 # include <Siv3D/Window/IWindow.hpp>
 # include <Siv3D/GLFW/GLFW.hpp>
@@ -35,11 +36,59 @@ namespace s3d
 		const String& getWindowTitle() const noexcept override;
 
 		void* getHandle() const noexcept override;
+		
+		const WindowState& getState() const noexcept override;
+
+		void setStyle(WindowStyle style) override;
+
+		void setPos(Point pos) override;
+
+		void show() override;
+
+		void maximize() override;
+
+		void restore() override;
+
+		void minimize() override;
+
+		bool resizeByVirtualSize(Size virtualSize) override;
+
+		bool resizeByFrameBufferSize(Size frameBufferSize) override;
+
+		void setMinimumFrameBufferSize(Size size) override;
+
+		void setFullscreen(bool fullscreen, size_t monitorIndex) override;
+		
+		void setToggleFullscreenEnabled(bool enabled) override;
+
+		bool isToggleFullscreenEnabled() const override;
 
 	private:
 
 		GLFWwindow* m_glfwWindow = nullptr;
 		
 		WindowTitle m_windowTitle;
+		
+		WindowState m_state;
+		
+		Stopwatch m_moveResizeStabilizer;
+		
+		Rect m_storedWindowRect = { 0, 0, 0, 0 };
+
+		void updateState();
+		
+		static void OnMove(GLFWwindow* glfwWindow, int x, int y);
+		
+		static void OnResize(GLFWwindow* glfwWindow, int width, int height);
+		
+		static void OnFrameBufferSize(GLFWwindow* glfwWindow, int width, int height);
+		
+		static void OnScalingChange(GLFWwindow* glfwWindow, float sx, float sy);
+		
+		static void OnIconify(GLFWwindow* glfwWindow, int iconified);
+		
+		static void OnMaximize(GLFWwindow* glfwWindow, int maximized);
+		
+		static void OnFocus(GLFWwindow* glfwWindow, int focused);
 	};
 }
---
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/Window/WindowMisc.cpp
@@ -0,0 +1,22 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2024 Ryo Suzuki
+//	Copyright (c) 2016-2024 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include "WindowMisc.hpp"
+# include <Siv3D/GLFW/GLFW.hpp>
+
+namespace s3d::WindowMisc
+{
+	// TODO: implement
+	int32 GetTitleBarHeight(GLFWwindow* glfwWindow)
+	{
+		return 0;
+	}
+}
