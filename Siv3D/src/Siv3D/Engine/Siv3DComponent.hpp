1b57312e0e98d81a050d2f3e135ca606bec4f372
yaito3014 <ykakeyama3014@gmail.com>
2025-04-08 03:27:49 +0900

    add missing include

--- Siv3D/src/Siv3D/Engine/Siv3DComponent.hpp
+++ Siv3D/src/Siv3D/Engine/Siv3DComponent.hpp
@@ -12,6 +12,31 @@
 # pragma once
 # include <cassert>
 
+#include "Siv3D/LicenseManager/ILicenseManager.hpp"
+#include "Siv3D/Logger/ILogger.hpp"
+#include "Siv3D/RegExp/IRegExp.hpp"
+#include "Siv3D/System/ISystem.hpp"
+#include "Siv3D/EngineResource/IEngineResource.hpp"
+#include "Siv3D/Profiler/IProfiler.hpp"
+#include "Siv3D/AssetMonitor/IAssetMonitor.hpp"
+#include "Siv3D/UserAction/IUserAction.hpp"
+#include "Siv3D/Window/IWindow.hpp"
+#include "Siv3D/Scene/IScene.hpp"
+#include "Siv3D/ImageDecoder/IImageDecoder.hpp"
+#include "Siv3D/ImageEncoder/IImageEncoder.hpp"
+#include "Siv3D/Emoji/IEmoji.hpp"
+#include "Siv3D/Console/IConsole.hpp"
+#include "Siv3D/Cursor/ICursor.hpp"
+#include "Siv3D/CursorStyle/ICursorStyle.hpp"
+#include "Siv3D/Keyboard/IKeyboard.hpp"
+#include "Siv3D/Mouse/IMouse.hpp"
+#include "Siv3D/Renderer/IRenderer.hpp"
+#include "Siv3D/Texture/ITexture.hpp"
+#include "Siv3D/Shader/IShader.hpp"
+#include "Siv3D/EngineShader/IEngineShader.hpp"
+#include "Siv3D/Renderer2D/IRenderer2D.hpp"
+#include "Siv3D/ScreenCapture/IScreenCapture.hpp"
+
 namespace s3d
 {
 	template <class Interface>
